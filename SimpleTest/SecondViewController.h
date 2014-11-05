f728068faaad64052808c9b7dc5942a08b533518
Maxime Austruy <austruym@gmail.com>
2014-11-05 08:39:27 +0100

    Initial Commit

---
+++ SimpleTest/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  SimpleTest
+//
+//  Created by Maxime Austruy on 05/11/14.
+//  Copyright (c) 2014 Maxime Austruy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
---
+++ SimpleTest/SecondViewController.m
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.m
+//  SimpleTest
+//
+//  Created by Maxime Austruy on 05/11/14.
+//  Copyright (c) 2014 Maxime Austruy. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
